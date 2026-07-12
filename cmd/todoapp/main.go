12647bd289425d5f68b97f7b34843c160227ec94
_Rich_ <Rich@example.com>
2026-07-12 20:42:18 +0300

    fixes

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	core_config "github.com/PhitonBedrosovich/golang-todoapp/internal/core/config"
 	core_logger "github.com/PhitonBedrosovich/golang-todoapp/internal/core/logger"
 	core_pgx_pool "github.com/PhitonBedrosovich/golang-todoapp/internal/core/repository/postgres/conn/pgx"
 	core_http_middleware "github.com/PhitonBedrosovich/golang-todoapp/internal/core/transport/http/middleware"
@@ -21,12 +22,9 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	timeZone = time.UTC
-)
-
 func main() {
-	time.Local = timeZone
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -41,7 +39,7 @@ func main() {
 	}
 	defer logger.Close()
 
-	logger.Debug("application time zone", zap.Any("zone", timeZone))
+	logger.Debug("application time zone", zap.Any("zone", time.Local))
 
 	// DEBUG log, чтобы понимать когда начинается инициализация connection pool
 	logger.Debug("initializing postgres connection pool")
